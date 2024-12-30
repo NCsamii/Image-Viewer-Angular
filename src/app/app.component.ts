c5669e7df2fa7ac9a9e54fbb4a88275c205ba7b7
ncsamii <ncsameie@gmail.com>
2024-12-30 13:43:11 +0330

    refactor: refactor functions in imageViewer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: false
 })
 export class AppComponent {
   title = 'imageViewer';
-  images:string[]=['assets/img/image-1.jpg' , 'assets/img/image-2.jpg', 'assets/img/image-3.jpg' , 'assets/img/image-4.jpg']
+  images: string[] = ['assets/img/image-1.jpg', 'assets/img/image-2.jpg', 'assets/img/image-3.jpg', 'assets/img/image-4.jpg']
 }
+
--- src/app/components/image-viewer/image-viewer.component.ts
+++ src/app/components/image-viewer/image-viewer.component.ts
@@ -1,14 +1,13 @@
 import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
-    selector: 'app-image-viewer',
-    templateUrl: './image-viewer.component.html',
-    styleUrls: ['./image-viewer.component.scss'],
-    standalone: false
+  selector: 'app-image-viewer',
+  templateUrl: './image-viewer.component.html',
+  styleUrls: ['./image-viewer.component.scss'],
+  standalone: false
 })
 export class ImageViewerComponent implements OnInit {
   @ViewChild('imageContainer') imageContainer !: ElementRef;
-  @Output() imageIndex = new EventEmitter<any>()
 
   @Input() set images(image: string[]) {
     this.imageList = image
@@ -32,18 +31,18 @@ export class ImageViewerComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  next(): void {
+  public next(): void {
     this.currentIndex = (this.currentIndex + 1) % this.imageList.length;
     this.reset()
   }
 
-  prev(): void {
+  public prev(): void {
     this.currentIndex = (this.currentIndex - 1 + this.imageList.length) % this.imageList.length;
     this.reset()
   }
 
   // RESET STYLED AFTER NEXT OR PREV ACTION
-  reset() {
+  private reset(): void {
     this.scale = 1
     this.rotate = 1
     this.rotateLeft = 360
@@ -53,7 +52,7 @@ export class ImageViewerComponent implements OnInit {
   }
 
   // SET FULL SCREEN IMAGE
-  full() {
+  public full(): void {
     if (!document.fullscreenElement) {
       const image = this.imageContainer.nativeElement.querySelector('.slider-image.active');
       image.requestFullscreen()
@@ -63,43 +62,43 @@ export class ImageViewerComponent implements OnInit {
   }
 
   // ZOOM BASED ON MOUSE
-  zoom(event: any) {
+  public zoom(event: WheelEvent) {
     event.preventDefault();
     const xs = (event.clientX - this.pointX) / this.scale;
     const ys = (event.clientY - this.pointY) / this.scale;
-    const delta = (event.wheelDelta ? event.wheelDelta : -event.deltaY);
+    const delta = (event.deltaY ? event.deltaY : -event.deltaY);
     (delta > 0) ? (this.scale *= 1.2) : (this.scale /= 1.2);
 
     this.setTransform();
   }
 
   // ZOOM IN BASED ON BUTTON
-  zoomIn() {
+  public zoomIn(): void {
     this.scale += 0.2;
     this.setTransform();
   }
 
   // ZOOM OUT BASED ON BUTTON
-  zoomOut() {
+  public zoomOut(): void {
     this.scale -= 0.2;
     this.setTransform();
   }
 
   // ROTATE RIGHT TO LEFT
-  rotateRTL() {
+  public rotateRTL(): void {
     this.rotate = this.rotate + 10;
     this.setTransform();
   }
 
   // ROTATE LEFT TO RIGHT
-  rotateLTR() {
+  public rotateLTR(): void {
     console.log(this.rotate)
     this.rotate = this.rotate - 10;
     this.setTransform();
   }
 
   // ROTATE RIGHT TO LEFT WITH 90 DEG
-  rotate90RTL() {
+  public rotate90RTL(): void {
     if (this.rotate % 90 !== 0) {
       this.rotate = Math.ceil(this.rotate / 90) * 90;
 
@@ -110,7 +109,7 @@ export class ImageViewerComponent implements OnInit {
   }
 
   // ROTATE LEFT TO RIGHT WITH 90 DEG
-  rotate90LTR() {
+  public rotate90LTR(): void {
     if (this.rotate % 90 !== 0) {
       this.rotate = Math.ceil(this.rotate / -90) * -90;
     } else {
@@ -120,14 +119,14 @@ export class ImageViewerComponent implements OnInit {
   }
 
   // FOR MOVING IMAGE
-  mouseDown(event: any) {
+  public mouseDown(event: MouseEvent): void {
     const test = document.getElementsByClassName('slider-image')
     event.preventDefault();
     this.start = {x: event.clientX - this.pointX, y: event.clientY - this.pointY};
     this.panning = true;
   }
 
-  mouseMove(event: any) {
+  public mouseMove(event: MouseEvent): void {
 
     event.preventDefault();
     if (!this.panning) {
@@ -138,7 +137,7 @@ export class ImageViewerComponent implements OnInit {
     this.setTransform();
   }
 
-  setTransform() {
+  public setTransform(): void {
     const image = this.imageContainer.nativeElement.querySelector('.slider-image.active');
     image.style.transform = 'translate(' + this.pointX + 'px, ' + this.pointY + 'px) scale(' + this.scale + ')  rotate(' + this.rotate + 'deg)';
   }
